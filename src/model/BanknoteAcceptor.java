9417efe729a296caaa52272bbd1b5cc7f5f4f2e0
Timur T <neoftality@gmail.com>
2025-04-18 11:20:22 +0600

    Add new class BanknoteAcceptor

---
+++ src/model/BanknoteAcceptor.java
@@ -0,0 +1,25 @@
+package model;
+
+public class BanknoteAcceptor implements MoneyAcceptor {
+
+    private int amount;
+
+    public BanknoteAcceptor(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String getName() {
+        return "Купюр";
+    }
+}
